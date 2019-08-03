17b587e39f97a9a7dd978b680fe9fbb6e482c70f
Taillook <jousoku@gmail.com>
2019-08-03 16:01:42 +0900

    add project

---
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by Taillook on 2019/07/28.
+//  Copyright © 2019 Taillook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
